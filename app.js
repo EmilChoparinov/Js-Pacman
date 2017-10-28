1a2d666b3a3372bfd60c98913c3d397b8e83e75e
EmilChoparinov <emilchoparinov@gmail.com>
2017-10-28 15:27:41 -0700

    Added ghosts and their AI

--- app.js
+++ app.js
@@ -4,20 +4,37 @@ var DOT = '<img src="dot.png" alt="dot id="dot">';
 var PAC = '<img src="pacman.gif" alt="pacman" id="pacman">'
 var ghosts = {
     'blinky': {
-        'image':'<img src="blinky.jpg" alt="blinky" id="blinky">',
-        'id': 4
+        'name': 'blinky',
+        'image': '<img src="blinky.jpg" alt="blinky" id="blinky">',
+        'id': 4,
+        'row': 9,
+        'col': 8,
+        'lrow': 9,
+        'lcol': 8
     },
     'pinky': {
-        'image':'<img src="pinky.jpg" alt="pinky" id="pinky">',
-        'id': 5
+        'image': '<img src="pinky.jpg" alt="pinky" id="pinky">',
+        'id': 5,
+        'row': 3,
+        'col': 16,
+        'lrow': 3,
+        'lcol': 16
     },
-    'inky':{
-        'image':'<img src="inky.jpg" alt="inky" id="inky">',
-        'id': 6
+    'inky': {
+        'image': '<img src="inky.jpg" alt="inky" id="inky">',
+        'id': 6,
+        'row': 16,
+        'col': 3,
+        'lrow': 16,
+        'lcol': 3
     },
     'clyde': {
-        'image':'<img src="clyde.jpg" alt="clyde" id="clyde">',
-        'id': 7
+        'image': '<img src="clyde.jpg" alt="clyde" id="clyde">',
+        'id': 7,
+        'row': 10,
+        'col': 17,
+        'lrow': 10,
+        'lcol': 17
     }
 }
 
@@ -29,7 +46,7 @@ var PAC_COR = {
 var world = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 2, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
     [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -43,12 +60,20 @@ var world = [
     [1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, ghosts.blinky.id, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
     [1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
     [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
+world[PAC_COR.row][PAC_COR.col] = 2;
+// world[ghosts.blinky.row][ghosts.blinky.col] = ghosts.blinky.id;
+// world[ghosts.pinky.row][ghosts.pinky.col] = ghosts.pinky.id;
+
+for (ghost in ghosts) {
+    console.log(ghosts[ghost]);
+    world[ghosts[ghost].row][ghosts[ghost].col] = ghosts[ghost].id;
+}
 function drawWorld() {
     var nextFrame = '';
     for (var row = 0; row < world.length; row++) {
@@ -57,21 +82,144 @@ function drawWorld() {
             else if (world[row][col] == 1) nextFrame += WALL;
             else if (world[row][col] == 2) nextFrame += PAC;
             else if (world[row][col] == 3) nextFrame += GROUND;
-            else if(world[row][col] == ghosts.blinky.id) nextFrame += ghosts.blinky.image;
-            else if(world[row][col] == ghosts.clyde.id) nextFrame += ghosts.clyde.image;
-            else if(world[row][col] == ghosts.inky.id) nextFrame += ghosts.inky.image;
-            else if(world[row][col] == ghosts.pinky.id) nextFrame += ghosts.pinky.image;
+            else if (world[row][col] == ghosts.blinky.id) nextFrame += ghosts.blinky.image;
+            else if (world[row][col] == ghosts.clyde.id) nextFrame += ghosts.clyde.image;
+            else if (world[row][col] == ghosts.inky.id) nextFrame += ghosts.inky.image;
+            else if (world[row][col] == ghosts.pinky.id) nextFrame += ghosts.pinky.image;
         }
     }
     $('#wrapper').html(nextFrame);
 }
 
+function ghostAlg(ghost) {
+    var canMove = canMovePlaces(ghost);
+    console.log(`(row: ${ghost.row}, col: ${ghost.col}), (lrow: ${ghost.lrow}, lcol: ${ghost.lcol})`);
+    if (canMove.down) canMove.down = calc(ghost, 'down');
+    if (canMove.up) canMove.up = calc(ghost, 'up');
+    if (canMove.right) canMove.right = calc(ghost, 'right');
+    if (canMove.left) canMove.left = calc(ghost, 'left');
+    console.log(canMove);
+    for (var i = 0; i < Object.keys(canMove).length; i++) {
+        if (typeof canMove[Object.keys(canMove)[i]] == 'boolean') {
+            canMove[Object.keys(canMove)[i]] = Number.MAX_VALUE;
+        }
+    }
+    var closestPlace = {
+        'cur': Number.MAX_VALUE,
+        'type': ''
+    }
+    for (mem in canMove) {
+        if (closestPlace.cur > canMove[mem]) {
+            closestPlace.type = mem;
+            closestPlace.cur = canMove[mem];
+        }
+    }
+    moveTo(ghost, closestPlace.type);
+}
+function moveTo(ghost, closestPlace) {
+    switch (closestPlace) {
+        case 'right':
+            ghost.lcol = ghost.col;
+            ghost.lrow = ghost.row;
+            world[ghost.row][ghost.col + 1] = ghost.id;
+            world[ghost.row][ghost.col] = 0;
+            ghost.col++;
+            drawWorld();
+            $(`#${ghost.name}`).css('transform', 'scaleX(1)');
+            break;
+        case 'left':
+            ghost.lcol = ghost.col;
+            ghost.lrow = ghost.row;
+            world[ghost.row][ghost.col - 1] = ghost.id;
+            world[ghost.row][ghost.col] = 0;
+            ghost.col--;
+            drawWorld();
+            $(`#${ghost.name}`).css('transform', 'scaleX(-1)');
+            break;
+        case 'down':
+            ghost.lrow = ghost.row;
+            ghost.lcol = ghost.col;
+            world[ghost.row + 1][ghost.col] = ghost.id;
+            world[ghost.row][ghost.col] = 0;
+            ghost.row++;
+            drawWorld();
+            break;
+        case 'up':
+            ghost.lrow = ghost.row;
+            ghost.lcol = ghost.col;
+            world[ghost.row - 1][ghost.col] = ghost.id;
+            world[ghost.row][ghost.col] = 0;
+            ghost.row--;
+            drawWorld();
+            break;
+    }
+}
+
+function canMovePlaces(ghost) {
+    var places = {
+        'left': false,
+        'right': false,
+        'up': false,
+        'down': false
+    };
+    if (world[ghost.row][ghost.col + 1] != 1 && ghost.col + 1 != ghost.lcol) {
+        var nextCor = world[ghost.row][ghost.col + 1];
+        places.right = true;
+        for (g in ghosts) {
+            if (nextCor == g.id) places.right = false;
+        }
+    }
+    if (world[ghost.row][ghost.col - 1] != 1 && ghost.col - 1 != ghost.lcol) {
+        var nextCor = world[ghost.row][ghost.col - 1];
+        places.left = true;
+        for (g in ghosts) {
+            if (nextCor == g.id) places.left = false;
+        }
+    }
+    if (world[ghost.row + 1][ghost.col] != 1 && ghost.row + 1 != ghost.lrow) {
+        var nextCor = world[ghost.row + 1][ghost.col];
+        places.down = true;
+        for (g in ghosts) {
+            if (nextCor == g.id) places.left = false;
+        }
+    }
+    if (world[ghost.row - 1][ghost.col] != 1 && ghost.row - 1 != ghost.lrow) {
+        var nextCor = world[ghost.row - 1][ghost.col];
+        places.up = true;
+        for (g in ghosts) {
+            if (nextCor == g.id) places.up = false;
+        }
+    }
+    return places;
+}
+
+function calc(ghost, place) {
+    switch (place) {
+        case 'up':
+            return Math.abs(ghost.col - PAC_COR.col) + Math.abs(ghost.row - 1 - PAC_COR.row);
+            break;
+        case 'down':
+            return Math.abs(ghost.col - PAC_COR.col) + Math.abs(ghost.row + 1 - PAC_COR.row);
+            break
+        case 'right':
+            return Math.abs(ghost.col + 1 - PAC_COR.col) + Math.abs(ghost.row - PAC_COR.row);
+            break;
+        case 'left':
+            return Math.abs(ghost.col - 1 - PAC_COR.col) + Math.abs(ghost.row - PAC_COR.row);
+            break;
+    }
+}
+
 $(document).ready(function () {
     drawWorld();
+    setInterval(function () { ghostAlg(ghosts.blinky) }, 600);
+    setInterval(function () { ghostAlg(ghosts.inky) }, 600);
+    setInterval(function () { ghostAlg(ghosts.clyde) }, 600);
+    setInterval(function () { ghostAlg(ghosts.pinky) }, 600);
+
 });
 
 $(document).on('keydown', 'body', function (e) {
-
     switch (e.keyCode) {
         case 39:
             if (world[PAC_COR.row][PAC_COR.col + 1] == 1) break;
@@ -98,7 +246,7 @@ $(document).on('keydown', 'body', function (e) {
             $('#pacman').css('transform', 'rotate(90deg)');
             break;
         case 38:
-            if(world[PAC_COR.row - 1][PAC_COR.col] == 1) break;
+            if (world[PAC_COR.row - 1][PAC_COR.col] == 1) break;
             world[PAC_COR.row - 1][PAC_COR.col] = 2;
             world[PAC_COR.row][PAC_COR.col] = 3;
             PAC_COR.row--;
